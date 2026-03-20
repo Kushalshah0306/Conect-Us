5545894c055f5521137410be782ab9f1e6cfbafa
Aryan jha <152834383+aryanjha205@users.noreply.github.com>
2026-03-20 18:12:43 +0530

    Add files via upload

--- static/sw.js
+++ static/sw.js
@@ -1,10 +1,11 @@
-const CACHE_NAME = 'conect-us-v1';
+const CACHE_NAME = 'conect-us-v2';
 const ASSETS_TO_CACHE = [
   '/',
   '/static/css/style.css',
-  '/static/manifest.json',
+  '/manifest.json',
   '/static/img/logo.png',
-  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
+  '/static/img/icon-192.png',
+  '/static/img/icon-512.png'
 ];
 
 self.addEventListener('install', event => {
@@ -13,19 +14,44 @@ self.addEventListener('install', event => {
       .then(cache => {
         console.log('Opened cache');
         return cache.addAll(ASSETS_TO_CACHE);
-      })
+      }),
   );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames =>
+      Promise.all(
+        cacheNames
+          .filter(cacheName => cacheName !== CACHE_NAME)
+          .map(cacheName => caches.delete(cacheName))
+      )
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') {
+    return;
+  }
+
   event.respondWith(
     caches.match(event.request)
       .then(response => {
-        // Cache hit - return response
         if (response) {
           return response;
         }
-        return fetch(event.request);
+        return fetch(event.request).then(networkResponse => {
+          if (!networkResponse || networkResponse.status !== 200 || networkResponse.type !== 'basic') {
+            return networkResponse;
+          }
+
+          const responseToCache = networkResponse.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put(event.request, responseToCache));
+          return networkResponse;
+        });
       }
     )
   );
